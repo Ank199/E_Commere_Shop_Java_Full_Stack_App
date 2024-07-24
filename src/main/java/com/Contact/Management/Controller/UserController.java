fed03615c83659f6a7b59d1c74e4efc5ba3f2696
Ank199 <97876169+Ank199@users.noreply.github.com>
2024-07-24 11:10:49 +0530

    Update UserController.java

--- src/main/java/com/Contact/Management/Controller/UserController.java
+++ src/main/java/com/Contact/Management/Controller/UserController.java
@@ -1,7 +1,6 @@
 package com.Contact.Management.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.Contact.Management.Models.User;
 import com.Contact.Management.Repository.UserRepository;
 import com.Contact.Management.global.GlobalData;
@@ -21,55 +20,54 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/user")
 public class UserController {
 
-
-  @Autowired
-  private CategoryService categoryService;
-  
-  @Autowired
-  private ProductService productService;
-  
-  @Autowired
-  private UserRepository userRepository;
-
-   @ModelAttribute
-  public void addCommonData(Model model, Principal principal) {
-    String userName = principal.getName();
-    System.out.println("UserName " + userName);
-    User user = userRepository.getUserByUsername(userName);
-    System.out.println("User " + user);
-    model.addAttribute("user", user);
-
-  }
-  
-  @GetMapping("/index")
-  public String Home(Model model,Principal principal) {
+    @Autowired
+    private CategoryService categoryService;
     
-      return "/index";
-  }
+    @Autowired
+    private ProductService productService;
+    
+    @Autowired
+    private UserRepository userRepository;
 
-  @GetMapping("/shop")
-  public String Shop(Model model) {
-    model.addAttribute("categories", this.categoryService.getAllCategory());
-    model.addAttribute("products", productService.GetAllProduct());
-     model.addAttribute("cartCount", GlobalData.cart.size());
-    return "/Shop/shop";
-  }
-  @GetMapping("/shop/category/{id}")
-  public String ShopByCategory(Model model,@PathVariable int id) {
-    model.addAttribute("categories", this.categoryService.getAllCategory());
-    model.addAttribute("products", productService.getAllProductByCategoryId(id));
-    // model.addAttribute("cartCount", GlobalData.cart.size());
-    return "/Shop/shop";
-  }
-  @GetMapping("/shop/viewproduct/{id}")
-  public String ViewProductById(Model model,@PathVariable Long id) {
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
+        if (principal != null) {
+            String userName = principal.getName();
+            System.out.println("UserName " + userName);
+            User user = userRepository.getUserByUsername(userName);
+            System.out.println("User " + user);
+            model.addAttribute("user", user);
+        }
+    }
 
-    model.addAttribute("product", productService.getProductById(id).get());
-    model.addAttribute("cartCount", GlobalData.cart.size());
+    @GetMapping("/index")
+    public String home(Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login"; // Redirect to login if the user is not authenticated
+        }
+        return "index";
+    }
 
-    return "/Shop/viewProduct";
-  }
-  
+    @GetMapping("/shop")
+    public String shop(Model model) {
+        model.addAttribute("categories", this.categoryService.getAllCategory());
+        model.addAttribute("products", productService.GetAllProduct());
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        return "Shop/shop";
+    }
 
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(Model model, @PathVariable int id) {
+        model.addAttribute("categories", this.categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductByCategoryId(id));
+        return "Shop/shop";
+    }
+
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProductById(Model model, @PathVariable Long id) {
+        model.addAttribute("product", productService.getProductById(id).orElse(null));
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        return "Shop/viewProduct";
+    }
 }
 
