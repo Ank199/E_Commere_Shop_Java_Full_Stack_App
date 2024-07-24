bedf60f3391647a3b11ad8e6bfed9b5214657ec7
Ank199 <97876169+Ank199@users.noreply.github.com>
2024-07-24 10:41:00 +0530

    Update UserController.java

--- src/main/java/com/Contact/Management/Controller/UserController.java
+++ src/main/java/com/Contact/Management/Controller/UserController.java
@@ -1,5 +1,11 @@
 package com.Contact.Management.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Contact.Management.Models.User;
@@ -10,65 +16,60 @@ import com.Contact.Management.services.ProductService;
 
 import java.security.Principal;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-
 @Controller
-
+@RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private CategoryService categoryService;
 
-  @Autowired
-  private CategoryService categoryService;
-  
-  @Autowired
-  private ProductService productService;
-  
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private ProductService productService;
 
-   @ModelAttribute
-  public void addCommonData(Model model, Principal principal) {
-    String userName = principal.getName();
-    System.out.println("UserName " + userName);
-    User user = userRepository.getUserByUsername(userName);
-    System.out.println("User " + user);
-    model.addAttribute("user", user);
+    @Autowired
+    private UserRepository userRepository;
 
-  }
-  
-  @GetMapping("/user/index")
-  public String Home(Model model,Principal principal) {
-    
-      return "/Shop/index";
-  }
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
+        if (principal != null) {
+            String userName = principal.getName();
+            User user = userRepository.getUserByUsername(userName);
+            if (user != null) {
+                model.addAttribute("user", user);
+            } else {
+                model.addAttribute("user", new User());
+            }
+        }
+    }
 
-  @GetMapping("/user/shop")
-  public String Shop(Model model) {
-    model.addAttribute("categories", this.categoryService.getAllCategory());
-    model.addAttribute("products", productService.GetAllProduct());
-     model.addAttribute("cartCount", GlobalData.cart.size());
-    return "/Shop/shop";
-  }
-  @GetMapping("/user/shop/category/{id}")
-  public String ShopByCategory(Model model,@PathVariable int id) {
-    model.addAttribute("categories", this.categoryService.getAllCategory());
-    model.addAttribute("products", productService.getAllProductByCategoryId(id));
-    // model.addAttribute("cartCount", GlobalData.cart.size());
-    return "/Shop/shop";
-  }
-  @GetMapping("/user/shop/viewproduct/{id}")
-  public String ViewProductById(Model model,@PathVariable Long id) {
+    @GetMapping("/index")
+    public String home(Model model, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login"; // Redirect to login if the user is not authenticated
+        }
+        return "Shop/index";
+    }
 
-    model.addAttribute("product", productService.getProductById(id).get());
-    model.addAttribute("cartCount", GlobalData.cart.size());
+    @GetMapping("/shop")
+    public String shop(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProduct());
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        return "Shop/shop";
+    }
 
-    return "/Shop/viewProduct";
-  }
-  
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(Model model, @PathVariable int id) {
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductByCategoryId(id));
+        return "Shop/shop";
+    }
 
+    @GetMapping("/shop/viewproduct/{id}")
+    public String viewProductById(Model model, @PathVariable Long id) {
+        model.addAttribute("product", productService.getProductById(id).orElse(null));
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        return "Shop/viewProduct";
+    }
 }
+
